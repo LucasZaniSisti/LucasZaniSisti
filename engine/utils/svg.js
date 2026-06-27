10cde1a7043cac38d78bb2a4d3941414aa6f58f0
Lucas Zani Sisti <115500318+LucasZaniSisti@users.noreply.github.com>
2026-06-27 18:02:45 -0300

    Create svg.js

---
+++ engine/utils/svg.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function createSVG(width, height) {
+    return `
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="${width}"
+     height="${height}"
+     viewBox="0 0 ${width} ${height}">
+`;
+}
+
+export function closeSVG(svg) {
+    return svg + "\n</svg>";
+}
+
+export function save(svg) {
+
+    const output = path.join(__dirname, "..", "..", "output");
+
+    fs.mkdirSync(output, { recursive: true });
+
+    fs.writeFileSync(
+        path.join(output, "starwars.svg"),
+        closeSVG(svg)
+    );
+}
