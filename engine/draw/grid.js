863f94733de37904df1cf94f69f01bb1ab012f61
Lucas Zani Sisti <115500318+LucasZaniSisti@users.noreply.github.com>
2026-06-27 22:35:28 -0300

    Update grid.js

--- engine/draw/grid.js
+++ engine/draw/grid.js
@@ -1,8 +1,35 @@
-export function drawGrid() {
+export function drawGrid(){
+
+    const weeks = 53;
+    const days = 7;
+
+    const cell = 12;
+    const gap = 5;
+
+    const margin = 25;
 
     let svg = "";
 
-    // desenha o grid
+    for(let x=0;x<weeks;x++){
+
+        for(let y=0;y<days;y++){
+
+            const px = margin + x*(cell+gap);
+            const py = margin + y*(cell+gap);
+
+            svg += `
+<rect
+x="${px}"
+y="${py}"
+width="${cell}"
+height="${cell}"
+rx="2"
+fill="#0e4429"/>
+`;
+
+        }
+
+    }
 
     return svg;
 
