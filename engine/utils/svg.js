cabc0b3cd9772197cd612424ffec67e53f8bc34a
Lucas Zani Sisti <115500318+LucasZaniSisti@users.noreply.github.com>
2026-06-27 22:34:02 -0300

    Update svg.js

--- engine/utils/svg.js
+++ engine/utils/svg.js
@@ -6,26 +6,37 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export function createSVG(width, height) {
+
     return `
-<svg xmlns="http://www.w3.org/2000/svg"
-     width="${width}"
-     height="${height}"
-     viewBox="0 0 ${width} ${height}">
+<svg
+xmlns="http://www.w3.org/2000/svg"
+width="${width}"
+height="${height}"
+viewBox="0 0 ${width} ${height}">
 `;
+
 }
 
-export function closeSVG(svg) {
+export function closeSVG(svg){
+
     return svg + "\n</svg>";
+
 }
 
-export function save(svg) {
+export function save(svg){
 
-    const output = path.join(__dirname, "..", "..", "output");
+    const output = path.join(
+        __dirname,
+        "..",
+        "..",
+        "output"
+    );
 
-    fs.mkdirSync(output, { recursive: true });
+    fs.mkdirSync(output,{recursive:true});
 
     fs.writeFileSync(
-        path.join(output, "starwars.svg"),
+        path.join(output,"starwars.svg"),
         closeSVG(svg)
     );
+
 }
