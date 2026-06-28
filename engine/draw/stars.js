13eedd2c3db836e230bcbe23235f1da3d82af9c4
Lucas Zani Sisti <115500318+LucasZaniSisti@users.noreply.github.com>
2026-06-27 22:44:40 -0300

    Update stars.js

--- engine/draw/stars.js
+++ engine/draw/stars.js
@@ -1,5 +1,32 @@
 export function drawStars(){
 
-    return "";
+    let svg = "";
+
+    const stars = 120;
+
+    const width = 951;
+    const height = 169;
+
+    for(let i=0;i<stars;i++){
+
+        const x = Math.random() * width;
+        const y = Math.random() * height;
+
+        const r = Math.random() * 1.5 + 0.2;
+
+        const opacity = Math.random() * 0.8 + 0.2;
+
+        svg += `
+<circle
+cx="${x}"
+cy="${y}"
+r="${r}"
+fill="white"
+opacity="${opacity}"/>
+`;
+
+    }
+
+    return svg;
 
 }
