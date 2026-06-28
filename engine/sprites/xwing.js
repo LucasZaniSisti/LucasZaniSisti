28abb044507135ea9bce02515a12f5bd2ffec45b
Lucas Zani Sisti <115500318+LucasZaniSisti@users.noreply.github.com>
2026-06-27 23:20:55 -0300

    Update xwing.js

--- engine/sprites/xwing.js
+++ engine/sprites/xwing.js
@@ -8,7 +8,43 @@ points="
 0,-10
 14,0
 0,10
+-5,4export function drawXWing() {
+
+return `
+<g>
+
+<g>
+
+<polygon
+points="
+0,-10
+14,0
+0,10
 -5,4
+-5,-4"
+fill="#dddddd"/>
+
+<circle
+cx="-3"
+cy="0"
+r="2"
+fill="#00ccff"/>
+
+<animateMotion
+dur="8s"
+repeatCount="indefinite"
+rotate="auto">
+
+<mpath href="#flightPath"/>
+
+</animateMotion>
+
+</g>
+
+</g>
+`;
+
+}
 -5,-4"
 fill="#dddddd"/>
 
