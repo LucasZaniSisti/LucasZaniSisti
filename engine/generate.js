3a61c52c611bc9472fb6196237610c44ed4f0e6a
Lucas Zani Sisti <115500318+LucasZaniSisti@users.noreply.github.com>
2026-06-27 22:38:04 -0300

    Update generate.js

--- engine/generate.js
+++ engine/generate.js
@@ -1,12 +1,20 @@
 import { createSVG, save } from "./utils/svg.js";
 
+import { drawBackground } from "./draw/background.js";
 import { drawGrid } from "./draw/grid.js";
+import { drawStars } from "./draw/stars.js";
 
-const width = 926;
+const width = 951;
 const height = 169;
 
-let svg = createSVG(width, height);
+let svg = createSVG(width,height);
+
+svg += drawBackground();
 
 svg += drawGrid();
 
+svg += drawStars();
+
 save(svg);
+
+console.log("Engine OK");
